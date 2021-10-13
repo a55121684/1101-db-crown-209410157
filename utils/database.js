2dca7e86ef8ce4324e1b4b99f15bc953ce6fc01a
Shin <bear55121677@gmail.com>
2021-10-13 20:25:46 +0800

    2021/10/13 add heroku

--- utils/database.js
+++ utils/database.js
@@ -1,16 +1,21 @@
 const { Pool } = require("pg");
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "crown_57",
-  password: "0000",
-  port: "5432",
+  // user: "postgres",
+  // host: "localhost",
+  // database: "crown_57",
+  // password: "0000",
+  // port: "5432",
+  connectionString: isProduction
+    ? process.env.DATABASE_URL
+    : `postgresql://postgres:0000@localhost:5432/crown57`,
 });
 
-pool.query("SELECT * from category_57", (err, res) => {
-  console.log(JSON.stringify(res.rows));
-  pool.end();
-});
+// pool.query("SELECT * from category_57", (err, res) => {
+//   console.log(JSON.stringify(res.rows));
+//   pool.end();
+// });
 
 module.exports = pool;
